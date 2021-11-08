997c3e8d20c94f3427ec043ed4844649766d2bc3
Craig Barcinas <cbarcinas@live.com>
2021-11-07 19:19:50 -0800

    actionsRouter completed

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,45 @@
 // Write your "actions" router here!
+const actionsRouter = require("express").Router();
+const Actions = require("./actions-model");
+const { validateId, validateBody } = require("./actions-middlware");
+actionsRouter.get("/", async (req, res, next) => {
+  try {
+    const actions = await Actions.get();
+    res.status(200).json(actions);
+  } catch (error) {
+    next(error);
+  }
+});
+actionsRouter.get("/:id", validateId, async (req, res, next) => {
+  try {
+    res.status(200).json(req.actionId);
+  } catch (error) {
+    next(error);
+  }
+});
+actionsRouter.post("/", validateBody, async (req, res, next) => {
+  try {
+    await Actions.insert(req.action);
+    res.status(201).json(req.action);
+  } catch (error) {
+    next(error);
+  }
+});
+actionsRouter.put("/:id", validateId, validateBody, async (req, res, next) => {
+  try {
+    const updateAction = await Actions.update(req.params.id, req.action);
+    res.status(200).json(updateAction);
+  } catch (error) {
+    next(error);
+  }
+});
+actionsRouter.delete("/:id", validateId, async (req, res, next) => {
+  try {
+    await Actions.remove(req.actionId.id);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = actionsRouter;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,40 @@
-const express = require('express');
+// Configure your server here
+const express = require("express");
+
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
 const server = express();
 
-// Configure your server here
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+server.use(morgan("dev"));
+
 // Build your actions router in /api/actions/actions-router.js
+const actionsRouter = require("./actions/actions-router");
+server.use("/api/actions", actionsRouter);
+
 // Build your projects router in /api/projects/projects-router.js
+const projectsRouter = require("./projects/projects-router");
+server.use("/api/projects", projectsRouter);
+
 // Do NOT `server.listen()` inside this file!
+
+const currentTime = new Date().toLocaleTimeString();
+
+server.get("/", (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: `Server is running as of ${currentTime}`,
+  });
+});
+
+// server.use((err, req, res, next) => {
+//   res.status(err.status || 500).json({
+//     message: err.message,
+//   });
+// });
 
 module.exports = server;
